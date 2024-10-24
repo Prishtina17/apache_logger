784a15f0b87cfe910be922d2c3eee417e347ee53
Prishtina17 <52797266+Prishtina17@users.noreply.github.com>
2024-10-24 22:09:38 +0300

    bebra

---
+++ config.py
@@ -0,0 +1,11 @@
+file_paths = [('logs1.txt','%h,%t,%r,%>s,%b'),
+              ('logs2.txt','%r,%>s')]
+db_info = {
+            'database': 'postgres',
+            'user': 'postgres',
+            'password': 'postgres',
+            'host': '127.0.0.1',
+            'port': '5432'
+}
+
+
